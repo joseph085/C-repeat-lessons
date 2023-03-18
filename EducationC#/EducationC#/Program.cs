4a94be1acddac209b81697800bffd2c5aec11481
Vagif Pashayev (PC) <pashayev.vaqif2005@rambler.ru>
2023-03-18 19:18:29 +0400

    Added new solutions

---
+++ EducationC#/EducationC#/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EducationC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///////////////Data types///////////////////
+
+            //bool heYadaYox = false; // true
+            //string ad = "Salam";
+            //char herf = 'A';
+            //int reqem = 5;
+            //float natamamEded = 1.4254f;
+            //double dahaBoyukNatamamEded = 1.4256216231d;
+            //decimal heddinanArtiqBoyukNatamamEded = 1.35258239581895891235185981351818M;
+
+            /////////////////////////////////////////////
+        }
+    }
+}
