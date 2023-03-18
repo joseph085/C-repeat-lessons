ee3aecf8be9e3e40bbe0a6fc6550f97230158951
Vagif Pashayev (PC) <pashayev.vaqif2005@rambler.ru>
2023-03-18 20:11:25 +0400

    Added new things

--- EducationC#/EducationC#/Program.cs
+++ EducationC#/EducationC#/Program.cs
@@ -15,6 +15,38 @@
             //decimal heddinanArtiqBoyukNatamamEded = 1.35258239581895891235185981351818M;
 
             /////////////////////////////////////////////
+            //     == (beraber)  != (beraber deyil)  || (ve ya)  && (ve)   ! (deyil)   <    >   <=   >=
+
+            //Example:
+            Console.Write("Enter the number: ");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a % 4 == 0)
+            {
+                Console.WriteLine("Even");      //12    13    14    15    16    17    18    19    20
+            }
+            else if (a % 4 == 1)
+            {
+                Console.WriteLine("Odd");
+            }
+            else
+            {
+                Console.WriteLine("Salam");
+            }
+
+
+            //for (int i = 0; i < 15; i++)
+            //{
+            //    Console.WriteLine("daga");
+            //}
+            //while (a < 5)
+            //{
+            //    Console.WriteLine("sda");
+            //}
+            //do
+            //{
+            //    Console.WriteLine("sd");
+            //} while (a + 5 < 51);
         }
     }
 }
