1008dc9975b289c39a12ca38b939f444859787d6
joseph085 <huseynzadeyusiff@gmail.com>
2023-03-19 14:39:14 +0400

    verilen task icrasi

--- EducationC#/EducationC#/Program.cs
+++ EducationC#/EducationC#/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace EducationC_
+﻿using System.Transactions;
+
+namespace EducationC_
 {
     internal class Program
     {
@@ -18,21 +20,31 @@
             //     == (beraber)  != (beraber deyil)  || (ve ya)  && (ve)   ! (deyil)   <    >   <=   >=
 
             //Example:
-            Console.Write("Enter the number: ");
-            int a = int.Parse(Console.ReadLine());
+            // Console.Write("Enter the number: ");
+            // int a = int.Parse(Console.ReadLine());
 
-            if (a % 4 == 0)
-            {
-                Console.WriteLine("Even");      //12    13    14    15    16    17    18    19    20
-            }
-            else if (a % 4 == 1)
-            {
-                Console.WriteLine("Odd");
-            }
-            else
-            {
-                Console.WriteLine("Salam");
-            }
+            // if (a % 4 == 0)
+            // {
+            //     Console.WriteLine("Even");      //12    13    14    15    16    17    18    19    20
+            // }
+            // else if (a % 4 == 1)
+            // {
+            //     Console.WriteLine("Odd");
+            // }
+            // else if (a % 4 == 2)
+            // {
+            //     Console.WriteLine("ok");
+            // }
+            //else if  (a % 4 == 3)
+            // {
+            //     Console.WriteLine("Odin");
+            // }
+            // else  
+            // {
+            //     Console.WriteLine("Salam");
+
+            // }
+
 
 
             //for (int i = 0; i < 15; i++)
@@ -47,6 +59,52 @@
             //{
             //    Console.WriteLine("sd");
             //} while (a + 5 < 51);
+
+            ///// New Task \\\\\\
+
+            //string givemyName = "Enter the command :" + Console.ReadLine();
+            //string hello = "Salam";
+            //string enter = "Enter the command :";
+            //string exit = "Cixis :";
+
+            //while (enter == enter )
+            //{
+            //    Console.WriteLine();
+            //}
+
+            #region yeni task
+            string command;
+             while (true)
+            {
+                Console.WriteLine("Enter command:");
+                command = Console.ReadLine();
+
+                if (command == "/givemyname")
+                {
+                    Console.WriteLine("Yusif");
+                }
+                else if (command == "/hello")
+                {
+                    Console.WriteLine("Salam");
+                }
+                else if (command == "/hellohowareyou")
+                {
+                    Console.WriteLine("Unknown command. Available commands: /hello, /givemyname, /exit");
+                }
+                else if  (command == "/exit")
+                {
+                    Console.WriteLine("Exit program :");
+                }
+                
+
+            }
+            #endregion
+
+
+           
+
+
+
         }
     }
 }
